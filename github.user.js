3f0a9d0f53498371e604d60bc8ba3612086d684b
John Long <a@88k.us>
2014-02-13 03:04:20 -0600

    Add github compact issue script.

---
+++ github.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name          Github - Compact issues
+// @namespace     http://www.88k.us
+// @description   Compact issues
+// @match         https://www.github.com/*
+// @match         https://github.com/*
+// ==/UserScript==
+
+GM_addStyle(".list-group-item { padding: 2px 10px 2px 32px; }");
+GM_addStyle(".list-group-item-name { font-size: 13px; margin-bottom: -1px; }");
